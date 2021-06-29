e4032ed5e40a47a907a91dd91fcde9dce1b2f281
Fufu Fang <fangfufu2003@gmail.com>
2021-06-29 02:34:50 +0100

    simplifying code

--- Physical-Mute-Button.ino
+++ Physical-Mute-Button.ino
@@ -2,33 +2,18 @@
 #include "M5Atom.h"
 #include "BleKeyboard.h"
 
-uint8_t DisBuff[2 + 5 * 5 * 3];
 BleKeyboard bleKeyboard("Mute Button", "M5Stack Atom Matrix", 100);;
-
-void setBuff(uint8_t Rdata, uint8_t Gdata, uint8_t Bdata)
-{
-    DisBuff[0] = 0x05;
-    DisBuff[1] = 0x05;
-    for (int i = 0; i < 25; i++)
-    {
-        DisBuff[2 + i * 3 + 0] = Rdata;
-        DisBuff[2 + i * 3 + 1] = Gdata;
-        DisBuff[2 + i * 3 + 2] = Bdata;
-    }
-}
 
 void setup()
 {
     M5.begin(false, false, true);
     delay(10);
-    setBuff(0x00, 0x00, 0x00);
-    M5.dis.displaybuff(DisBuff);
-
+    M5.dis.fillpix(CRGB::Black);
     bleKeyboard.begin();
 }
 
 bool muted = false;
-int blue_level = 0;
+int brightness = 0;
 int increment = 0;
 void loop()
 {
@@ -41,23 +26,24 @@ void loop()
       delay(100);
       bleKeyboard.releaseAll();
     }
+    M5.dis.setBrightness(100);
     if (muted) {
-      setBuff(0xff, 0x00, 0x00);
+      M5.dis.fillpix(CRGB::Green);
     } else {
-      setBuff(0x00, 0x00, 0x00);
+      M5.dis.fillpix(CRGB::Black);
     }
   } else {
     muted = false;
-    if (blue_level < 10) {
-      increment = 10;
-    } else if (blue_level > 240) {
-      increment = -10;
+    M5.dis.fillpix(CRGB::Blue);
+    if (brightness <= 0) {
+      increment = 2;
+    } else if (brightness >= 100) {
+      increment = -2;
     }
-    blue_level += increment;
-    setBuff(0x00, 0x00, blue_level);
+    brightness += increment;
+    M5.dis.setBrightness(brightness);
   }
   // Don't ask me why these have to be in the loop, they just do.
-  M5.dis.displaybuff(DisBuff);
   M5.update();
   delay(50);
 }
